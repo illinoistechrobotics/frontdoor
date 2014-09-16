59a7ae602b750f26bdd15f44649d59f556c5bb9f
Peter Chinetti <peter@chinetti.me>
2014-09-16 12:43:01 -0500

    seems like this is pretty much all we need

--- upstairs/console.py
+++ upstairs/console.py
@@ -1,13 +1,30 @@
 import math
+import redis
 import Adafruit_CharLCD as LCD
+
 
 def read_card(bits):
     cardreader = open('/dev/prox','r')
-    data = cardreader.read(math.ceiling(bits/8.0))
+    data = cardreader.read(math.ceil(bits/8.0))
     #bits can be unaligned, so we need to fix them
-    pass
+    bitstring = "{0:b}".format(data)
+    bitstring = bitstring[len(bitstring) - bits:]
+    return bitstring
+
 def write_card_lcd(card_number, name, status):
-    #lcd.
-    pass
-def lookup_card(card_number):
-    pass
+    lcd.message(name + '\n' + status)
+
+
+def lookup_card(card_number, redis):
+    pipe =  redis.pipeline()
+    status = ''
+    if pipe.sismember("IN", card_number):
+        pipe.srem("IN", card_number)
+        status = 'OUT'
+    else:
+        pipe.sadd("IN", card_number)
+        status = 'IN'
+    pipe.execute()
+    name = redis.get(card_number + "_name")
+    return (name, status)
+
