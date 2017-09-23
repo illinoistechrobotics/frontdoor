8686018cd36509808e59cdaaa01acb54e16c9c37
Taylor Berg <me@tman0.com>
2017-09-23 03:38:52 -0500

    Port to Linux. Remove dependency on VideoCapture, update tkinter and replace PIL with Pillow. Update to Python 3.

--- upstairs/gui.py
+++ upstairs/gui.py
@@ -1,24 +1,25 @@
-from Tkinter import *
-import serial
-import redis
+#!/usr/bin/python
 import logging
-import time
-from VideoCapture import Device
-import PIL
+import os
+from tkinter import *
+
+import PIL.Image
 import PIL.ImageTk
 import prox
+import redis
+import serial
 
 log = logging.getLogger(__name__)
-#On the laptop, the Arduino enumerates as COM3
-#I've specified the baud rate to be max (115200)
+# On the laptop, the Arduino enumerates as COM3
+# I've specified the baud rate to be max (115200)
 
-ser = serial.Serial("COM3", 115200) #I'm not going to specify a timeout so I can be blocking on the serial port
+ser = serial.Serial("/dev/ttyACM0",
+                    115200)  # I'm not going to specify a timeout so I can be blocking on the serial port
 red = redis.StrictRedis(host='localhost', port=6379, db=0)
 
 class PhotoEntry:
     def __init__(self, master, id_num):
-        self.cam = Device()
-        self.id_num = id_num 
+        self.id_num = id_num
         
         self.frame = Frame(master)
         self.frame.pack()
@@ -40,25 +41,28 @@ class PhotoEntry:
 
     def grab_name(self):
         self.name = self.name_box.get()
-        time.sleep(5)
-        img = self.cam.getImage()
-        self.img_string = img.tostring()
-        img.thumbnail((800,600), PIL.Image.ANTIALIAS)
+
+        os.system('mpv --length=5 /dev/video0')
+
+        os.system('ffmpeg -f v4l2 -i /dev/video0 -video_size 1280x720 -vframes 1 temp_image.png -y')
+
+        img = PIL.Image.open("temp_image.png")
         photo = PIL.ImageTk.PhotoImage(img)
         if self.picture is not None:
             self.picture.destroy()
         self.picture = Label(self.frame, image=photo)
         self.picture.image = photo
         self.picture.pack()
+        self.img_string = img.tobytes()
         self.photo_button.config(state=NORMAL)
         self.instructions.config(text="If you are happy with the photo, click 'Submit Photo', otherwise take a new photo")
 
     def submit(self):
-        if self.name is None:
+        if self.name is None or self.img_string is "":
             return
-        red.set(self.id_num+".name", self.name)
-        red.set(self.id_num+".status", 1)
-        red.set(self.id_num+".img",self.img_string)
+        red.set(str(self.id_num) + ".name", self.name)
+        red.set(str(self.id_num) + ".status", 1)
+        red.set(str(self.id_num) + ".img", self.img_string)
         red.sadd("inlab",self.id_num)
         self.frame.quit()
 
@@ -66,12 +70,12 @@ class Login:
     def __init__(self,master,id_num):
         frame = Frame(master)
         frame.pack()
-        
-        topline = red.get(id_num+".name")+"\n["+id_num+"]"
+
+        topline = "%s\n[%s]" % (red.get("%s.name" % str(id_num)), str(id_num))
         nameline = Label(frame, text=topline, bg="black", fg="white", font = "Monospace 30")
         nameline.pack(fill=X)
 
-        status = int(red.get(id_num+'.status'))
+        status = int(red.get(str(id_num) + '.status'))
         if status % 2:
             statusline = Label(frame, text="CHECKIN", bg="black", fg="green", font="Monospace 30")
             statusline.pack(fill=X)
@@ -81,10 +85,9 @@ class Login:
             statusline.pack(fill=X)
             red.srem("inlab",id_num)
 
-        img_str = red.get(id_num+'.img')
+        img_str = red.get(str(id_num) + '.img')
         if img_str is not None:
-            img = PIL.Image.fromstring('RGB',(1280,720),img_str)
-            img.thumbnail((800,600), PIL.Image.ANTIALIAS)
+            img = PIL.Image.frombytes('RGB', (1280, 720), img_str)
             photo = PIL.ImageTk.PhotoImage(img)
             picture = Label(frame, image=photo)
             picture.image = photo
@@ -114,11 +117,11 @@ def run_loop():
         log.error("Parity error: ignoring")
         return
     #Check redis for the user with that id
-    name = red.get(id_num+'.name')
+    name = red.get(str(id_num) + '.name')
     if name is not None:
         #If the user exists, toggle their in lab status and display that to them
         #Send an update hook to the itr website
-        status = red.incr(id_num+'.status')
+        status = red.incr(str(id_num) + '.status')
         display_login(id_num)
     else:
         #If they don't exist, prompt them to add themselves
